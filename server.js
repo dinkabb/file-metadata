dd3149149760d1af81de9be69e500bb28032660e
Dinka Bekavac <dinka.bekavac@gmail.com>
2022-03-03 18:50:38 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var cors = require("cors");
+let multer = require("multer");
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+let upload = multer({ dest: "uploads/" });
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  function (req, res, next) {
+    let upfile = req.file;
+    if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+    return res.json({
+      name: upfile.originalname,
+      type: upfile.mimetype,
+      size: upfile.size,
+    });
+  }
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
